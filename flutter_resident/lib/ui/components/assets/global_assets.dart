6aed11bb3d16971197227b356933f22bc86ffd5e
Matheus Moraes <matheus7mm@hotmail.com>
2022-07-17 13:08:55 -0300

    Adding launch icons for each environment

--- flutter_resident/lib/ui/components/assets/global_assets.dart
+++ flutter_resident/lib/ui/components/assets/global_assets.dart
@@ -1,4 +1,4 @@
 class GlobalAssets {
-  static String get errorSvg => 'lib/ui/assets/icon/error.svg';
-  static String get validSvg => 'lib/ui/assets/icon/valid.svg';
+  static String get errorSvg => 'lib/ui/assets/icons/error.svg';
+  static String get validSvg => 'lib/ui/assets/icons/valid.svg';
 }
